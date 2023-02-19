58e92033ad31405ed781cf51fb8bd2787b43d9bf
mohaimensark <mohaimensarkerofficial@gmail.com>
2023-02-19 19:26:57 +0600

    ether

---
+++ encryptKey.js
@@ -0,0 +1,27 @@
+const ethers = require("ethers");
+const fs = require("fs-extra"); // for reading file
+require("dotenv").config();
+
+//encrypKey.js is for encrypting the .env file . We are gonne make it more secure
+
+async function main(){
+   
+    const wallet = new ethers.Wallet(
+        process.env.PRIVATE_KEY
+      );
+    
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASAWORD,
+        process.env.PRIVATE_KEY
+    );
+    console.log(encryptedJsonKey);
+    fs.writeFileSync("./.encryptedKey.json",encryptedJsonKey);
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
